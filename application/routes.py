4e2428a0d674cce2468af82516a0355f9efdd842
Victoria Lloyd <QAInstructorSky@gmail.com>
2025-06-23 12:15:14 +0100

    EDIT: 12 jokes, version 2

--- application/routes.py
+++ application/routes.py
@@ -27,5 +27,11 @@ joke_dict = {0: ["Why was Cinderella so bad a football?", "She kept running away
              2: ["Why did the bicycle fall over?", "Because it was two tired"],
              3: ["What do you call a sad strawberry?", "A blueberry!"],
              4: ["How do you organise a space party?", "You planet."],
-             5: ["What do cows read the most?", "Cattle-logs."]
+             5: ["What do cows read the most?", "Cattle-logs."],
+             6: ["What do clouds wear under their shorts?", "Thunder pants!"],
+             7: ["What did 0 say to 8?", "Nice belt."],
+             8: ["What did the drummer name her twin daughters?", "Anna 1, Anna 2."],
+             9: ["How does the moon cut his hair?", "Eclipse it."],
+             10: ["Why did the scarecrow win an award?", "Because he was outstanding in his field."],
+             11: ["What’s brown and sticky?", "A stick."]
              }
